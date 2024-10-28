74f6d7b4b071519e1f9ab0e6e643112bdfd836b9
steven <libo@rongma.com>
2024-10-28 10:32:37 +0800

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-10-24 16:04:16
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-10-24 18:05:54
+ * @LastEditTime: 2024-10-25 14:43:56
  * @FilePath: \admin-fronted\src\App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-// src/App.tsx
 // src/App.tsx
 import React from 'react';
 import AppRoutes from './routes/AppRoutes.tsx';
@@ -24,20 +23,21 @@ const App: React.FC = () => {
 
     return (
         <Layout>
-            <Header>
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal">
-                    {isLoggedIn && (
+            {isLoggedIn && (
+                <Header>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal">
                         <Menu.Item key="logout" onClick={handleLogout}>
                             退出
                         </Menu.Item>
-                    )}
-                </Menu>
-            </Header>
+                    </Menu>
+                </Header>
+            )}
             <AppRoutes />
         </Layout>
     );
 };
 
 export default App;
+
 
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,11 +2,10 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-10-24 16:12:45
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-10-24 18:08:52
+ * @LastEditTime: 2024-10-25 14:48:25
  * @FilePath: \admin-fronted\src\routes\AppRoutes.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-// src/routes/AppRoutes.tsx
 // src/routes/AppRoutes.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
@@ -25,33 +24,41 @@ const AppRoutes: React.FC = () => {
 
     return (
         <Router>
-            <Layout style={{ minHeight: '100vh' }}>
-                <Sider width={200}>
-                    <Menu mode="inline" defaultSelectedKeys={['1']}>
-                        {token && (
-                            <>
-                                <Menu.ItemGroup key="userGroup" title="用户管理" icon={<UserOutlined />}>
-                                    <Menu.Item key="1"><a href="/users">用户列表</a></Menu.Item>
-                                </Menu.ItemGroup>
-                                <Menu.ItemGroup key="orderGroup" title="订单管理" icon={<OrderedListOutlined />}>
-                                    <Menu.Item key="2"><a href="/orders">订单列表</a></Menu.Item>
-                                </Menu.ItemGroup>
-                            </>
-                        )}
+            {token ? (
+                <Layout style={{ minHeight: '100vh' }}>
+                    <Sider width={200}>
+                    <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={[]}>
+                        <Menu.SubMenu key="userGroup" title="用户管理" icon={<UserOutlined />}>
+                            <Menu.Item key="1">
+                                <a href="/users">用户列表</a>
+                            </Menu.Item>
+                        </Menu.SubMenu>
+
+                        <Menu.SubMenu key="orderGroup" title="订单管理" icon={<OrderedListOutlined />}>
+                            <Menu.Item key="2">
+                                <a href="/orders">订单列表</a>
+                            </Menu.Item>
+                        </Menu.SubMenu>
                     </Menu>
-                </Sider>
-                <Layout style={{ padding: '0 24px 24px' }}>
-                    <Content style={{ padding: 24, margin: 0, minHeight: 280 }}>
-                        <Routes>
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/home" element={token ? <Home /> : <Navigate to="/login" />} />
-                            <Route path="/users" element={token ? <UserList /> : <Navigate to="/login" />} />
-                            <Route path="/orders" element={token ? <OrderList /> : <Navigate to="/login" />} />
-                        </Routes>
-                    </Content>
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Content style={{ padding: 24, margin: 0, minHeight: 280 }}>
+                            <Routes>
+                                <Route path="/home" element={<Home />} />
+                                <Route path="/users" element={<UserList />} />
+                                <Route path="/orders" element={<OrderList />} />
+                                <Route path="*" element={<Navigate to="/home" />} />
+                            </Routes>
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
+            ) : (
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+            )}
         </Router>
     );
 };
